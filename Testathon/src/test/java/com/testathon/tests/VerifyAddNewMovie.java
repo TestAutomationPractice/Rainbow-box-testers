b23f6547eb534720e5e70cf7795f6128fe9e8580
ptkumar <ptkumar@lutron.com>
2019-09-28 14:42:40 +0530

    merge

--- Testathon/src/test/java/com/testathon/tests/VerifyAddNewMovie.java
+++ Testathon/src/test/java/com/testathon/tests/VerifyAddNewMovie.java
@@ -5,19 +5,22 @@ import org.testng.asserts.SoftAssert;
 
 import com.testathon.automation.core.DriverFactory;
 import com.testathon.automation.core.TestBaseQuestD;
-import com.web.pageobjects.HomePage;
-import com.web.pageobjects.MoviePage;
+import com.web.pageobjects.CommonPage;
 
-public class VerifyMoviePage extends TestBaseQuestD{
+public class VerifyAddNewMovie extends TestBaseQuestD{
 	static DriverFactory instance = DriverFactory.getInstance();
 
 	@Test(groups = {"Sanity"})
 	public void verifyMoviePage() {
 		
 		SoftAssert softAssert = new SoftAssert();
+		CommonPage.clickLoginLink();
 		
+		CommonPage.enterUsername();
+		CommonPage.enterPassword();
+		CommonPage.clickLogin();
+		CommonPage.addMovie(); 
 		
-		System.out.println(MoviePage.verifyMovieName("Avatar"));
 		
 		 softAssert.assertAll();
 		
