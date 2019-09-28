f4e623822d2451f96f41b8b4ad56c421f6f5ad44
ptkumar <ptkumar@lutron.com>
2019-09-28 11:04:56 +0530

    commit OR, ControlsPage

---
+++ Testathon/src/main/java/com/web/pageobjects/ControlsPage.java
@@ -0,0 +1,12 @@
+//By controlImagesLevel1 = MKeywords.findElement(pageName, "ControlsImagesLevel1");
+//WebElement ele = instance.getDriver().findElement(MKeywords.findElement(pageName, "AcceptCookies")));
+
+//public class ControlsPage {
+//	private static DriverFactory instance = DriverFactory.getInstance();
+//
+//	private static final String pageName = "Controls";
+//
+//	public static void compareImages() {
+
+//	}
+//}
